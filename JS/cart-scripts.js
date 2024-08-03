ebdcbef61b5a3900876e547b3589e092d9c62483
BaoHG <33833362+ghost1512@users.noreply.github.com>
2024-08-03 10:48:04 +0700

    add
    add

--- JS/cart-scripts.js
+++ JS/cart-scripts.js
@@ -44,3 +44,19 @@ function showSection(sectionId) {
 //         showSection('home'); // Hiển thị phần Men mặc định
 //     }
 // });
+function showmess(sectionId) {
+    // Ẩn tất cả các section
+    const sections = document.querySelectorAll('.section');
+    sections.forEach(section => {
+        section.classList.remove('active'); // Loại bỏ lớp active
+    });
+
+    // Hiển thị section được chọn
+    const activeSection = document.getElementById(sectionId);
+    activeSection.classList.add('active'); // Thêm lớp active cho section được chọn
+
+    history.pushState(null, null, '#' + sectionId);
+    setTimeout(function () {
+        location.reload();
+    }, 3000);
+}
